1f45eba38e0077d958ded989f626db35204bbf70
tokouyuuki <tttyuki1050@gmail.com>
2024-08-10 01:27:32 +0900

    launch a project

---
+++ PrivateTalkApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrivateTalkApp
+//
+//  Created by 都甲裕希 on 2024/08/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrivateTalkApp/PrivateTalkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivateTalkAppApp.swift
+//  PrivateTalkApp
+//
+//  Created by 都甲裕希 on 2024/08/10.
+//
+
+import SwiftUI
+
+@main
+struct PrivateTalkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
