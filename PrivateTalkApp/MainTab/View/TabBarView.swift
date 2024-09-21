ddf5a45343959e24cc77321b562ff75e8c129971
tokouyuuki <tttyuki1050@gmail.com>
2024-09-21 21:09:13 +0900

    Tabのレイアウト修正

--- PrivateTalkApp/MainTab/View/TabBarView.swift
+++ PrivateTalkApp/MainTab/View/TabBarView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 // MARK: - Constants
 private struct Constants {
-    static let TAB_BAR_VIEW_SPACING = 0.0
+    static let TAB_BAR_VIEW_SPACING = 15.0
     static let ITEM_IN_TAB_SPACING = 5.0
     static let IMAGE_IN_TAB_WIDTH = 25.0
     static let IMAGE_IN_TAB_HEIGHT = 25.0
     static let TAB_HEIGHT = 30.0
-    static let TAB_VERTICAL = 10.0
+    static let TAB_BOTTOM = 8.0
 }
 
 // MARK: - TabBarView
@@ -47,33 +47,30 @@ private struct CustomTabBar: View {
     @Binding var currentTab: Tab
     
     var body: some View {
-        GeometryReader { geometry in
-            HStack(spacing: Constants.TAB_BAR_VIEW_SPACING) {
-                ForEach(Tab.allCases, id: \.hashValue) { tab in
-                    Button {
-                        // 選択されたタブをデフォルトのタブに設定する
-                        currentTab = tab
-                    } label: {
-                        VStack(spacing: Constants.ITEM_IN_TAB_SPACING) {
-                            Image(systemName: tab.imageName)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: Constants.IMAGE_IN_TAB_WIDTH, 
-                                       height: Constants.IMAGE_IN_TAB_HEIGHT)
-                                .foregroundStyle(currentTab == tab ? Color.foreground : .gray)
-                            Text(tab.tabText)
-                                .font(.caption)
-                                .foregroundStyle(Color.foreground)
-                        }
-                        .frame(maxWidth: .infinity)
+        HStack(spacing: Constants.TAB_BAR_VIEW_SPACING) {
+            ForEach(Tab.allCases, id: \.hashValue) { tab in
+                Button {
+                    // 選択されたタブをデフォルトのタブに設定する
+                    currentTab = tab
+                } label: {
+                    VStack(spacing: Constants.ITEM_IN_TAB_SPACING) {
+                        Image(systemName: tab.imageName)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: Constants.IMAGE_IN_TAB_WIDTH,
+                                   height: Constants.IMAGE_IN_TAB_HEIGHT)
+                            .foregroundStyle(currentTab == tab ? Color.foreground : .gray)
+                        Text(tab.tabText)
+                            .font(.caption)
+                            .foregroundStyle(Color.foreground)
                     }
-                    .disabled(currentTab == tab)
+                    .frame(maxWidth: .infinity)
                 }
+                .disabled(currentTab == tab)
             }
-            .frame(maxWidth: .infinity)
         }
         .frame(height: Constants.TAB_HEIGHT)
-        .padding(.vertical, Constants.TAB_VERTICAL)
+        .padding(.bottom, Constants.TAB_BOTTOM)
     }
 }
 
